1308db17c8934a41d04c8f6c5e8a4ef7626f2ab8
Luke Franzke <lukefranzke@gmail.com>
2025-10-11 17:20:42 +0200

    changed default TTS model

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  textToSpeechModel: 0, // 0: en_GB-cori-high, 1:en_GB-alan-medium, 2:en_US-lessac-medium, 3: de_DE-thorsten-medium. Changing this value may cause an initial delay as the model is downloaded.
+  textToSpeechModel: 1, // 0: en_GB-cori-high, 1:en_GB-alan-medium, 2:en_US-lessac-medium, 3: de_DE-thorsten-medium. Changing this value may cause an initial delay as the model is downloaded.
   speechToTextModel: 0, // 0: small english, 1: medium english, 2: small german. Changing this value may cause an initial delay as the model is downloaded.
   // OPENAI_API_KEY: 'your-api-key-here'
   // WiFi Configuration (optional)
